a0ee6465a4827128bf549e97552a1c0851ae7e8e
hientoanlam <95153617+hientoanlam@users.noreply.github.com>
2023-07-17 20:16:36 +0700

    update project

--- front-end/src/components/ui/ModalForm.vue
+++ front-end/src/components/ui/ModalForm.vue
@@ -14,11 +14,7 @@
         </div>
         <div class="overflow-y-scroll max-h-72 mb-6">
             <pre class="whitespace-pre-line text-white text-justify text-base">
-            “luminaelixir” combines “lumina” (light) and “elixir” (magical potion) to represent a potent source of light, positivity, and healing. It means that I want to provide something for you to be happy and shiny.
-            
-            This bottle contains a lot of capsules prepared by me. Whenever you are in a bad mood, please feel free to pick up one capsule to read my uplifting message.
-            
-            I am not sure that you will be better after reading the message, but keep in mind that I will always be by your side.
+            {{msg}}
             </pre>
         </div>
     </div>
@@ -28,12 +24,12 @@
     import CloseButton from "@/components/ui/CloseButton.vue"
     defineProps({
         is_shown: {type: Boolean, default: false},
-        msg: String,
+        msg: {type: String, default: "meo meo"},
         title: String,
         color: {type: String, default: 'white'}
     })
+
 </script>
 
 <style scoped>
-
 </style>
